632f8c2d974ee28cfe5f73424e6e578e88d85259
Bruno Deferrari <utizoc@gmail.com>
2019-11-29 14:58:02 -0300

    Beginning of bomb placement algorithm

--- main.go
+++ main.go
@@ -18,12 +18,33 @@ type game struct {
 }
 
 func makeGame(width, height, bombs uint) *game {
-	return &game{
+	game := &game{
 		width:  width,
 		height: height,
 		board:  make([]int8, height*width),
 		bombs:  bombs,
 	}
+	placeBombs(game)
+	return game
+}
+
+func placeBombs(game *game) {
+	for i := uint(0); i < game.bombs; i++ {
+		placeBombAtRandom(game)
+	}
+}
+
+func placeBombAtRandom(game *game) {
+	// TODO
+	// - find random cell without a bomb
+	// - place bomb at cell
+	// - increment nearby-mines count for all neighbor cells without bombs
+}
+
+func incrementNeighbors(game *game, cell uint) {
+	// TODO
+	// - Find neighbors without bombs
+	// - Increase value by 1 on each
 }
 
 func main() {
